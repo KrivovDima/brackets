cab9ffbff2b241b737553ef96765e4e5e2a088d3
KrivovDima <krivovdima89@gmail.com>
2021-03-09 22:25:58 +0400

    feat: implement function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    const parseBracketsConfig = bracketsConfig.map((item) => {
+        return `${item[0]}${item[1]}`
+    });
+
+    let strCounter = str.length;
+
+    while (str !== '') {
+        parseBracketsConfig.forEach(element => {
+            str = str.replace(element, '');
+        });
+        if (strCounter === str.length) {
+            return false;
+        }
+        strCounter = str.length;
+    }
+    return true;
+
 }
+
+
+
+
+
+
+
